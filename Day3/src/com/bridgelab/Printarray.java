1d88d2ed10115292d51aeb7d05d124042b3a39bb
Anshul <you@example.com>
2022-08-29 18:33:24 +0530

    here we use array

---
+++ Day3/src/com/bridgelab/Printarray.java
@@ -0,0 +1,14 @@
+package com.bridgelab;
+
+public class Printarray {
+	
+	public static void main(String[] args) {
+		int [] arr = new int [] {1, 2, 3, 4, 5};
+		System. out. println("array is ");
+		for (int i = 0; i < arr. length; i++) {
+		System. out. print(arr[i] + " ");
+
+}
+	}
+
+}
